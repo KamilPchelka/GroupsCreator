61bf6431728dd715964832f00c74bebfef18482d
siemaeniu500 <siemaeniu500@gmail.com>
2017-09-29 22:12:40 +0200

    add enable generate button if user select class group

--- src/pl/kamilpchelka/codecool/groupscreator/eventhandlers/GroupSizeEventHandler.java
+++ src/pl/kamilpchelka/codecool/groupscreator/eventhandlers/GroupSizeEventHandler.java
@@ -2,11 +2,15 @@ package pl.kamilpchelka.codecool.groupscreator.eventhandlers;
 
 import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import pl.kamilpchelka.codecool.groupscreator.controllers.Controller;
 
 public class GroupSizeEventHandler implements EventHandler {
 
     @Override
     public void handle(Event event) {
-
+        Controller controller = Controller.getInstance();
+        Button generate = controller.getGenerate();
+        if (generate.isDisabled() && !controller.getNames().getItems().isEmpty()) generate.setDisable(false);
     }
 }
