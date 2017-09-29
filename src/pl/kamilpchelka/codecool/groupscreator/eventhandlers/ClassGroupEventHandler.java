960c808d75b02e8eb2f25635fdf9c98b183c24ed
siemaeniu500 <siemaeniu500@gmail.com>
2017-09-29 22:09:33 +0200

    fix enable generate if GroupSizeComboBox seelction is not null

--- src/pl/kamilpchelka/codecool/groupscreator/eventhandlers/ClassGroupEventHandler.java
+++ src/pl/kamilpchelka/codecool/groupscreator/eventhandlers/ClassGroupEventHandler.java
@@ -2,7 +2,6 @@ package pl.kamilpchelka.codecool.groupscreator.eventhandlers;
 
 import javafx.event.Event;
 import javafx.event.EventHandler;
-import javafx.scene.control.Button;
 import pl.kamilpchelka.codecool.groupscreator.controllers.Controller;
 import pl.kamilpchelka.codecool.groupscreator.entites.CodeCoolClass;
 import pl.kamilpchelka.codecool.groupscreator.enums.ClassGroup;
@@ -13,10 +12,17 @@ public class ClassGroupEventHandler implements EventHandler {
     @Override
     public void handle(Event event) {
         Controller controller = Controller.getInstance();
-        Button generate = controller.getGenerate();
         ClassGroup classGroup = controller.getClassGroupComboBox().getSelectionModel().getSelectedItem();
         CodeCoolClass codeCoolClass = controller.getClassComboBox().getSelectionModel().getSelectedItem();
         controller.getNames().getItems().setAll(codeCoolClass.getStudentMap().get(classGroup));
-        if (generate.isDisabled()) generate.setDisable(false);
+        String selection = controller.getGroupSizeComboBox().getSelectionModel().getSelectedItem();
+        try {
+            if (!selection.equalsIgnoreCase("null")) controller.getGenerate().setDisable(false);
+
+        } catch (Exception e) {
+
+        }
+
+
     }
 }
